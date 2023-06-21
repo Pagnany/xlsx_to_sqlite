7e4224bdacf9950beb6b82552de843ae7b4c4f7a
Pagnany <eric.schmale@gmx.de>
2023-06-21 07:55:12 +0200

    einlesen einer Datei. Spalten können ausgegeben werden.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use calamine::{open_workbook, Error, RangeDeserializerBuilder, Reader, Xlsx};
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    let now = Instant::now();
+    let mut i = 0;
+    //let mut excel: Xlsx<_> = open_workbook("file.xlsx").unwrap();
+    let mut excel: Xlsx<_> = open_workbook("filelarge.xlsx").unwrap();
+    if let Some(Ok(r)) = excel.worksheet_range("EIS-DTA") {
+        for row in r.rows() {
+            //println!("row[0]={:?}", row[0]);
+            let test = match row[0].as_i64() {
+                Some(v) => {
+                    i += 1;
+                    v
+                }
+                None => 0,
+            };
+            //println!("test={}", test);
+        }
+    }
+    let elapsed = now.elapsed();
+
+    println!("Dauer: {:?}", elapsed);
 }
