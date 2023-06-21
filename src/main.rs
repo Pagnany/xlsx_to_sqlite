8e37dcd8228483b8fde84f9255bcb18cfbbb2180
Pagnany <eric.schmale@gmx.de>
2023-06-21 09:05:13 +0200

    import gebinde und select aus sqlite

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,53 @@
 use calamine::{open_workbook, Error, RangeDeserializerBuilder, Reader, Xlsx};
+use rusqlite::Connection;
 use std::time::Instant;
 
 fn main() {
+    let mut conn = match Connection::open("./files/test.db") {
+        Ok(conn) => conn,
+        Err(e) => panic!("Error opening database: {:?}", e),
+    };
+    /* 
+    conn.execute(
+        "CREATE TABLE test (
+            id    INTEGER PRIMARY KEY,
+            name  TEXT NOT NULL
+        )",
+        (), // empty list of parameters.
+    ).unwrap();
+    */
+
     let now = Instant::now();
-    let mut i = 0;
+    let mut gebinde = Vec::new();
+
     //let mut excel: Xlsx<_> = open_workbook("file.xlsx").unwrap();
     let mut excel: Xlsx<_> = open_workbook("filelarge.xlsx").unwrap();
     if let Some(Ok(r)) = excel.worksheet_range("EIS-DTA") {
         for row in r.rows() {
-            //println!("row[0]={:?}", row[0]);
-            let test = match row[0].as_i64() {
-                Some(v) => {
-                    i += 1;
-                    v
-                }
-                None => 0,
-            };
+            gebinde.push(row[68].to_string());
+            gebinde.push(row[130].to_string());
             //println!("test={}", test);
         }
     }
+
+    let tx = conn.transaction().unwrap();
+    let mut stmt = tx.prepare("INSERT INTO test (name) VALUES (?1)").unwrap();
+    for geb in gebinde {
+        stmt.execute([geb]).unwrap();
+    }
+    stmt.finalize().unwrap();
+    tx.commit().unwrap();
+
+    let mut stmt = conn.prepare("SELECT distinct name FROM test").unwrap();
+    let gebinde: Vec<String> = stmt
+        .query_map([], |row| row.get(0))
+        .unwrap()
+        .map(|x| x.unwrap())
+        .collect();
+    for geb in gebinde {
+        println!("Gebinde: {}", geb);
+    }
+
     let elapsed = now.elapsed();
 
     println!("Dauer: {:?}", elapsed);
