7bf0ba407b692cb310f988cde3baea90a074b916
Eric <eric.schmale@gmx.de>
2023-06-23 15:25:35 +0200

    create db with number of columns

--- src/main.rs
+++ src/main.rs
@@ -7,31 +7,30 @@ fn main() {
         Ok(conn) => conn,
         Err(e) => panic!("Error opening database: {:?}", e),
     };
-    /*
-    conn.execute(
-        "CREATE TABLE test (
-            id    INTEGER PRIMARY KEY,
-            name  TEXT NOT NULL
-        )",
-        (), // empty list of parameters.
-    ).unwrap();
-    */
 
-    let mut sql_table_create = String::from("CREATE TABLE test (id INTEGER PRIMARY KEY, ");
+    let mut sql_table_create =
+        String::from("CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY ");
 
     let now = Instant::now();
 
     let mut i = 1;
-    let mut excel: Xlsx<_> = open_workbook("EIS-DTA_4500M.xlsx").unwrap();
+    let mut excel: Xlsx<_> = open_workbook("filelarge.xlsx").unwrap();
     if let Some(Ok(r)) = excel.worksheet_range("EIS-DTA") {
         for cell in r.used_cells() {
             if cell.0 == 0 {
-                sql_table_create.push_str(&format!("c{} TEXT NOT NULL, ", i));
+                sql_table_create.push_str(&format!(", c{} TEXT NOT NULL ", i));
                 i += 1;
             }
         }
     }
+    sql_table_create.push_str(")");
     println!("{}", sql_table_create);
+
+    match conn.execute(&sql_table_create, ()) {
+        Ok(_) => (),
+        Err(e) => panic!("Error creating table: {:?}", e),
+    }
+
     /*
     let tx = conn.transaction().unwrap();
     let mut stmt = tx.prepare("INSERT INTO test (name) VALUES (?1)").unwrap();
