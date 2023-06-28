2d30df2809718f881b7edb6cad53865229b2f7c2
Pagnany <eric.schmale@gmx.de>
2023-06-28 16:00:38 +0200

    Fehlerbeschreibungen

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,17 @@ use std::time::Instant;
 fn main() {
     let now = Instant::now();
 
-    let mut conn = match Connection::open("./files/test.db") {
+    let mut conn = match Connection::open("./files/data.db") {
         Ok(conn) => conn,
         Err(e) => panic!("Error opening database: {:?}", e),
     };
 
     let mut sql_table_create =
-        String::from("CREATE TABLE IF NOT EXISTS test (id text PRIMARY KEY ");
-    let mut sql_insert_into = String::from("INSERT INTO test VALUES (");
+        String::from("CREATE TABLE IF NOT EXISTS data (id text PRIMARY KEY ");
+    let mut sql_insert_into = String::from("INSERT INTO data VALUES (");
 
-    let mut excel: Xlsx<_> = open_workbook("EIS-DTA_4500M.xlsx").unwrap();
-    let xlsx_range = excel.worksheet_range("EIS-DTA").unwrap().unwrap();
+    let mut excel: Xlsx<_> = open_workbook("EIS-DTA_4500M.xlsx").expect("Datei kann nicht geöffnet werden");
+    let xlsx_range = excel.worksheet_range("EIS-DTA").expect("Arbeitsmappe nicht gefunden.").expect("Kann keine Daten aus der Arbeitsmappe lesen.");
 
     for i in 1..=xlsx_range.width() {
         sql_table_create.push_str(&format!(",c{} TEXT NOT NULL", i));
@@ -35,8 +35,8 @@ fn main() {
     sql_insert_into.pop();
     sql_insert_into.push_str(")");
 
-    let tx = conn.transaction().unwrap();
-    let mut stmt = tx.prepare(sql_insert_into.as_str()).unwrap();
+    let tx = conn.transaction().expect("Fehler beim Starten der Transaktion.");
+    let mut stmt = tx.prepare(sql_insert_into.as_str()).expect("Fehler beim Erstellen des Statements.");
 
     let mut werte: Vec<String> = Vec::new();
     let mut current_line = 0;
@@ -45,7 +45,7 @@ fn main() {
         if cell.0 == current_line {
             werte.push(cell.2.to_string());
         } else {
-            stmt.execute(params_from_iter(werte.iter())).unwrap();
+            stmt.execute(params_from_iter(werte.iter())).expect("Fehler beim Ausführen des Statements.");
 
             // erster Wert
             current_line += 1;
@@ -54,8 +54,8 @@ fn main() {
             werte.push(cell.2.to_string());
         }
     }
-    stmt.finalize().unwrap();
-    tx.commit().unwrap();
+    stmt.finalize().expect("Fehler beim Finalisieren des Statements.");
+    tx.commit().expect("Fehler beim Commiten der Transaktion.");
 
     let elapsed = now.elapsed();
 
